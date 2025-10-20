3537774daed0aa20f913537e70511099f4ca3d3c
Peterr7788 <ndabogunde@gmail.com>
2025-10-20 08:38:58 +0100

    Added new question 1

--- questions.js
+++ questions.js
@@ -2,3 +2,8 @@
 export const questions = [
   { id: 1, text: "Placeholder Q (will be replaced)", choices: ["A","B","C","D"], answerIndex: 0 }
 ];
+{
+  question: "What is the capital of Nigeria?",
+  answers: ["Abuja", "Lagos", "Kano", "Ibadan"],
+  correct: "Abuja"
+}
